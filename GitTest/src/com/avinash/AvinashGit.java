66a2b499cc78217b48e61180f2860fe5d53923e2
hugo <manojanand432@gmail.com>
2013-01-22 16:26:09 +0530

    secondcommit

--- GitTest/src/com/avinash/AvinashGit.java
+++ GitTest/src/com/avinash/AvinashGit.java
@@ -3,5 +3,23 @@ package com.avinash;
 public class AvinashGit {
  public static void main(String[] args) {
 	System.out.println("Avinash");
+	for (int i=0; i<10;i++)
+	{
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		System.out.println("this is avinsh");
+		
+	}
 }
 }
